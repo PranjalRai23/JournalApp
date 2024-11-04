e63e9a7e54d278b990d71d8a2e9bce19d9cbdef1
Pranjal Rai <pranjalrai1@gmail.com>
2024-11-05 02:45:56 +0530

    First commit

---
+++ src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
@@ -0,0 +1,28 @@
+package net.engineeringdigest.journalApp.entity;
+
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Document(collection = "journal_entries")
+@Getter
+@Setter
+//or we can use annotation @Data and @NoArgsConstructor
+public class JournalEntry {
+
+    @Id
+    private ObjectId id;
+    @NonNull
+    private String title;
+
+    private String content;
+
+    private LocalDateTime date;
+
+}
